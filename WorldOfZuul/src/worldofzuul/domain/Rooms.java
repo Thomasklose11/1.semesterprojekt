a844630fed3681da42c5dd0e53868b6ce08fbeea
Zuperp <stkro14@student.sdu.dk>
2018-12-04 11:19:10 +0100

    Updated room text

--- WorldOfZuul/src/worldofzuul/domain/Rooms.java
+++ WorldOfZuul/src/worldofzuul/domain/Rooms.java
@@ -48,35 +48,34 @@ public class Rooms {
                 + "obtaining the legendary jade statue”", "Rooms/F1_B1.png");
         f1_C1 = new Room("You spot on the floor, a whip lying in the dust.", "Rooms/F1_C1.png");
 
-        f1_A2 = new Room("On the floor you spot a treasure.","Rooms/F1_A2.png");
+        f1_A2 = new Room("","Rooms/F1_A2.png");
         f1_B2 = new Room("The door in front of you is locked, answer the statues puzzle to continue through.","Rooms/F1_B2.png", "Rooms/F1_B2U.png");
         f1_C2 = new Room("You enter the room and spot a large shadow out of the corner of your eye, before you can get a good look\n"
                 + "at it, it disappears.", "Rooms/F1_C2.png");
 
-        f1_A3 = new Room("On the floor you spot a valuable treasure and pick it up", "Rooms/F1_A3.png");
-        f1_B3 = new Room("You stand in front of a pit and see a large branch above it, it is out of your reach, but it looks stable and\n"
+        f1_A3 = new Room("", "Rooms/F1_A3.png");
+        f1_B3 = new Room("You stand in front of a pit and see a large branch on the other side of it, it is out of your reach, but it looks stable and\n"
                 + "solid.", "Rooms/F1_B3.png");
-        f1_C3 = new Room("You stand in an empty room.", "Rooms/F1_C3.png");
+        f1_C3 = new Room("", "Rooms/F1_C3.png");
 
         //Second floor rooms
-        f2_A1 = new Room("You stand in an empty room.");
-        f2_B1 = new Room("You have reached a checkpoint and have ventured deeper in the temple");
+        f2_A1 = new Room("");
+        f2_B1 = new Room("You feel your soul connecting with the gemstones in the floor. The way forward is deeper and darker than before.");
         f2_C1 = new Room("You’re standing in a dark room.");
 
         f2_A2 = new Room("You see a lever high up on the wall, it is out of your reach");
-        f2_B2 = new Room("As you enter the room the wall begins to close in and a riddle is presented to you, solve it in time or be\n"
-                + "crushed (WIP)");
-        f2_C2 = new Room("You stand in an empty room.");
-        f2_D2 = new Room("On the floor you spot a treasure and pick it up");
+        f2_B2 = new Room("");
+        f2_C2 = new Room("");
+        f2_D2 = new Room("");
 
         f2_A3 = new Room("You’re standing in a dark room.");
-        f2_B3 = new Room("You stand in an empty room.");
+        f2_B3 = new Room("");
         f2_C3 = new Room("The doors around you are locked, answer this puzzle correct to continue through.");
         f2_D3 = new Room("You see a button on the other side of a large gap, it is out of your reach .");
 
         f2_A4 = new Room("You see a torch hanging on the wall, do you want to pick it up?");
-        f2_B4 = new Room("You stand in an empty room.");
-        f2_C4 = new Room("Enemy room (WIP)");
+        f2_B4 = new Room("");
+        f2_C4 = new Room("");
         f2_D4 = new Room("You spot a gas mask on the corpse of a dead Nazi soldier.");
 
         f2_A5 = new Room("The only way forward is down through the hole in the floor, you see a branch sturdy enough to hold your weight.");
@@ -86,56 +85,56 @@ public class Rooms {
 
         //Third floor rooms
         f3_A1 = new Room("You stand in a dark room");
-        f3_B1 = new Room("You have reached a checkpoint and have ventured deeper in the temple");
+        f3_B1 = new Room("You feel your soul connect with the large gemstone once again.");
         f3_C1 = new Room("The door in front of you is locked, answer this puzzle correct to continue through.");
         f3_D1 = new Room("You are presented with a riddle, answer it to move on.");
-        f3_E1 = new Room("You have reached a checkpoint. \n"
-                + "In the same room you spot an inscription on the wall which you translate to: “A choice must be made, head west to help yourself down the line, or east if you are up for a rewarding challenge”.");
-        f3_F1 = new Room("A room with an enemy (WIP)");
+        f3_E1 = new Room("You feel the large gemstone connect with your soul once again. \n"
+                + "");
+        f3_F1 = new Room("");
 
-        f3_A2 = new Room("On the floor you spot a treasure and pick it up");
-        f3_B2 = new Room("You stand in an empty room");
+        f3_A2 = new Room("");
+        f3_B2 = new Room("");
         f3_C2 = new Room("You’re stuck in a room, and have to answer this riddle to move on.");
-        f3_D2 = new Room("You spot a green key, hanging on a tiny hook on the wall.");
-        f3_E2 = new Room("Trapped room (WIP)");
-        f3_F2 = new Room("On the floor you spot a valuable treasure and pick it up.");
+        f3_D2 = new Room("You spot a green key");
+        f3_E2 = new Room("");
+        f3_F2 = new Room("");
 
         f3_A3 = new Room("You stand in a dark room");
-        f3_B3 = new Room("Trapped room (WIP)");
-        f3_C3 = new Room("Enemy room (WIP)");
-        f3_D3 = new Room("You see a pair of guns lying on the floor, you’re also forced to solve a riddle to continue.");
-        f3_E3 = new Room("On the floor you spot a treasure and pick it up");
+        f3_B3 = new Room("");
+        f3_C3 = new Room("");
+        f3_D3 = new Room("You see a pair of guns lying on the floor.");
+        f3_E3 = new Room("");
         f3_F3 = new Room("A large lever is in the room, pulling it may open the shut door to the west");
 
         f3_A4 = new Room("The door in front of you is locked, answer this puzzle correct to continue through.");
-        f3_B4 = new Room("Hint (WIP)");
-        f3_C4 = new Room("On the floor you spot a treasure and pick it up");
-        f3_D4 = new Room("Health+ (WIP)");
-        f3_E4 = new Room("Health+ (WIP)");
-        f3_F4 = new Room("Indy's hat  (WIP)");
+        f3_B4 = new Room("");
+        f3_C4 = new Room("");
+        f3_D4 = new Room("");
+        f3_E4 = new Room("");
+        f3_F4 = new Room("You spot a somewhat familiar hat near the dead person");
 
-        f3_A5 = new Room("You find a blue key(WIP)");
-        f3_B5 = new Room("Health+ (WIP)");
-        f3_C5 = new Room("You stand in an empty room ");
-        f3_D5 = new Room("Enemy (WIP)");
-        f3_E5 = new Room("You stand in an empty room ");
+        f3_A5 = new Room("You find a blue key");
+        f3_B5 = new Room("");
+        f3_C5 = new Room("");
+        f3_D5 = new Room("");
+        f3_E5 = new Room("");
         f3_F5 = new Room("You’re stuck in a room, and have to answer this riddle to move on.");
 
-        f3_A6 = new Room("On the floor you spot a treasure and pick it up");
-        f3_B6 = new Room("Enemy(WIP)");
-        f3_C6 = new Room("Trap (WIP)");
-        f3_D6 = new Room("El pisko(WIP)");
-        f3_E6 = new Room("Bonus+ Health+  (WIP)");
-        f3_F6 = new Room("You stand in an empty room ");
+        f3_A6 = new Room("");
+        f3_B6 = new Room("");
+        f3_C6 = new Room("");
+        f3_D6 = new Room("The button to open the door is high up, and you can't reach it with your bare hands");
+        f3_E6 = new Room("");
+        f3_F6 = new Room("");
 
-        f3_A7 = new Room("Giant bonus treasure super room+++ (WIP)");
-        f3_B7 = new Room("You stand in an empty room ");
-        f3_C7 = new Room("On the floor you spot a treasure and pick it up");
-        f3_D7 = new Room("You stand in front of a huge door. You need a password to open the door");
-        f3_E7 = new Room("Trap room (WIP)");
+        f3_A7 = new Room("The bonus here seems unusally large, and definitely worth collecting");
+        f3_B7 = new Room("");
+        f3_C7 = new Room("");
+        f3_D7 = new Room("Answer this riddle to continue. You feel the pressure from behind the door, something is behind it.");
+        f3_E7 = new Room("");
         f3_F7 = new Room("You find a green key");
 
-        endRoom = new Room("Last room + Jade statue");
+        endRoom = new Room("The final room! On the pedestal in the middle of the room, you spot the long sought after Jade Statue");
 
         //First floor exits
         startRoom.setExit("north", f1_B1);
