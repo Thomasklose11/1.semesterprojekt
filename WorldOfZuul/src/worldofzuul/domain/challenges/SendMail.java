461f722819bf7cff41c41e4e534cb2696753123a
Morten <monoe18@student.sdu.dk>
2018-12-14 00:14:06 +0100

    Mail sender ikke txt men rum besøgt i besked

--- WorldOfZuul/src/worldofzuul/domain/challenges/SendMail.java
+++ WorldOfZuul/src/worldofzuul/domain/challenges/SendMail.java
@@ -5,6 +5,7 @@ import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.Properties;
 import javax.activation.*;
+import worldofzuul.domain.rooms.Rooms;
 
 
 public class SendMail {
@@ -37,26 +38,25 @@ public class SendMail {
 
             message.setSubject("Testing Subject");
 
-            message.setText("Dear Mail Crawler,"
-                    + "\n\n No spam to my email, please!");
-
-            Multipart multipart = new MimeMultipart();
-
-            BodyPart messageBodyPart = new MimeBodyPart();
-
-            messageBodyPart = new MimeBodyPart();
-
-            //VIRKER IKKE. ROOMCOUNTER.txt bliver muligvis ikke sendt til DataSource, men ved ikke nok om den datatype til at gennemskue fejlen
-            String filename =  "RoomCounter.txt";
-            DataSource source = new FileDataSource(filename);
-            
-            messageBodyPart.setDataHandler(new DataHandler(source));
-
-            messageBodyPart.setFileName(filename);
-
-            multipart.addBodyPart(messageBodyPart);
-
-            message.setContent(multipart);
+            message.setText("Rooms visited: " + Rooms.getRoomCounter());
+//
+//            Multipart multipart = new MimeMultipart();
+//
+//            BodyPart messageBodyPart = new MimeBodyPart();
+//
+//            messageBodyPart = new MimeBodyPart();
+//
+//            //VIRKER IKKE. ROOMCOUNTER.txt bliver muligvis ikke sendt til DataSource, men ved ikke nok om den datatype til at gennemskue fejlen
+//            String filename =  "RoomCounter.txt";
+//            DataSource source = new FileDataSource(filename);
+//            
+//            messageBodyPart.setDataHandler(new DataHandler(source));
+//
+//            messageBodyPart.setFileName(filename);
+//
+//            multipart.addBodyPart(messageBodyPart);
+//
+//            message.setContent(multipart);
 
             System.out.println("Sending");
 
